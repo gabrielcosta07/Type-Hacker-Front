708c49b83b147672099c6e1ee1c042898cfa5f4c
Gabriel Costa <gab75698@gmail.com>
2025-05-23 11:49:28 -0300

    escrevendo as div da page jogo

--- front-end/src/pages/Jogo/index.jsx
+++ front-end/src/pages/Jogo/index.jsx
@@ -4,9 +4,32 @@ import Campo_Digitar from "../../assets/Campo_Digitar.png";
 
 function Jogo() {
   return (
-    <div>
+    <div className="container-jogo">
       <img src={Fundo} alt="Background" className="backgroundJogo" />
-      <img src={Campo_Digitar} alt="Campo de digitação" className="campoDigitarJogo" />
+      <div className="sub-container">
+        <div className="palavras-chaves">
+          <h2>digite essa palavra</h2>
+        </div>
+        <div className="prompt"></div>
+        <div className="container-input">
+          <img
+            src={Campo_Digitar}
+            alt="Campo de digitação"
+            className="campoDigitarJogo"
+          />
+        </div>
+        <div className="timer">
+
+        </div>
+      </div>
+      <div className="container-lateral">
+        <div className="personagem">
+
+        </div>
+        <div className="palavras-digitadas">
+          
+        </div>
+      </div>
     </div>
   );
 }
