ab744ecbe6663b7c716f9db1c6c61d338e2c4995
CastellPg <reinaldoantoniocastellanoneto@gmail.com>
2025-06-01 00:38:33 -0300

    Tentativa Numero 1 de Fazer o jogo funcionar
    refazer por que esta dando errado <3 esqueci alguma parte

---
+++ src/pages/Jogo/Corpo Jogo.php
@@ -0,0 +1,28 @@
+<?php
+    $json_das_palavras = file_get_contents("palavras.json");
+    $lista_de_palavras_php = json_decode($json_das_palavras);
+    $tamanho_inicial_fonte = 20;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Zero-day</title>
+    <link rel="stylesheet" href="Corpo Css Jogo.css">
+</head>
+<body>
+
+    <h1 id="palavraAnimada"
+        style="font-size: <?php echo $tamanho_inicial_fonte; ?>px;">
+    </h1>
+
+    <div class="container-input">
+        <input type="text" id="campoControle" placeholder="Quebre o Codigo!" autofocus>
+    </div>
+
+    <script src="Corpo Javascript.js" defer></script>
+
+</body>
+</html>
