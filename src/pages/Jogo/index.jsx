f918e1c7ad192ebbb7da1c10ed61a987ad248390
CastellPg <reinaldoantoniocastellanoneto@gmail.com>
2025-06-02 12:45:59 -0300

    classes e etc melhoradas

--- src/pages/Jogo/index.jsx
+++ src/pages/Jogo/index.jsx
@@ -4,34 +4,33 @@ import Campo_Digitar from "../../assets/Campo_Digitar.png";
 
 function Jogo() {
   return (
-    <div className="container-jogo">
-      <img src={Fundo} alt="Background" className="backgroundJogo" />
-      <div className="sub-container">
-        <div className="palavras-chaves">
-          <h2>digite essa palavra</h2>
-        </div>
-        <div className="prompt"></div>
-        <div className="container-input">
-          <img
-            src={Campo_Digitar}
-            alt="Campo de digitação"
-            className="campoDigitarJogo"
-          />
-        </div>
-        <div className="timer">
+    <img src={Fundo} alt="Background" className="backgroundJogo" />
+   {/* parte esquerda onde vai aparecer o cmd e junto a ele, a barra de digitação */}
+  <div className="Esquerda"> 
+  
+    <div className="sub-container"> {/* parte que vai conectar tudo bonitinho, a palavra e tals */}
 
-        </div>
-      </div>
-      <div className="container-lateral">
-        <div className="personagem">
+            <div className="palavra-atual"></div> {/* palavra que desce e que tem q acertar*/}
 
-        </div>
-        <div className="palavras-digitadas">
-          
-        </div>
-      </div>
+            <div className="container-input"> {/*form/onde vai ser o Campo realmente*/}
+
+                <img src={Campo_Digitar} alt="Campo de digitação" className="campo-digitar"/>
+     
+            </div>
+        
+        <div className="timer"></div>  {/* literalmente o reloginho */}
+      
     </div>
-  );
-}
+    
+    <div className="Direita"></div>  {/* Parte em que vai ficar o personagem, log, barrinha e o manuel */}
+    
+    <div className="container-lateral">
+    
+        <div className="personagem"></div>
+        
+        <div className="palavras-digitadas"></div>
 
+        <div className=""></div>
+    </div>
+  </div>
 export default Jogo;
