b6f2124bad77397564f8783261540920c5becca6
Reinaldo Antonio Castellano Neto <reinaldoantoniocastellanoneto@gmail.com>
2025-06-06 23:05:37 -0300

    vamos ?

--- src/pages/Jogo/jogo.js
+++ src/pages/Jogo/jogo.js
@@ -2,8 +2,9 @@ const elementoPalavra = document.getElementsByClassName('palavra-atual')[0];
 const input = document.getElementsByClassName('input')[0];
 
 let posicaoVertical = 0;
-let velocidade = 2;
+let velocidade = 1;
 let intervalo = null;
+let morte = 0;
 
 if (elementoPalavra) {
     intervalo = setInterval(() => {
@@ -23,22 +24,55 @@ if (elementoPalavra) {
             if (input.value.toLowerCase() === elementoPalavra.innerText.toLowerCase()) {
                 clearInterval(intervalo);
                 input.disabled = true;
+                elementoPalavra.classList.add('shake-animation');
                 elementoPalavra.style.color = "green";
+
+                setTimeout(() => {
+                    elementoPalavra.classList.remove('shake-animation');
+                }, 200);
+
             } else {
+
                 elementoPalavra.style.color = "red";
                 input.value = "";
+                velocidade += 1;
+                morte += 1;
+                elementoPalavra.classList.add('shake-animation');
+                
+                    if (morte === 3) {
+                        clearInterval(intervalo);
+                        input.disabled = true;
+                        elementoPalavra.style.color = "red";
+                         setTimeout(() => {
+                             elementoPalavra.classList.remove('shake-animation');
+                        }, 200);
+                    } else {
+                        setTimeout(() => {
+                            elementoPalavra.style.color = "white";
+                            elementoPalavra.classList.remove('shake-animation');
+                        }, 200);
+                    }
             }
         }
     });
 }
-//trava tela
+
+// trava tela
 window.addEventListener('keydown', (event) => {
-    if ((event.ctrlKey || event.metaKey) && (event.key === '+' || event.key === '-' || event.key === '0')) {
+    if (
+        (event.ctrlKey || event.metaKey) &&
+        (event.key === '+' || event.key === '-' || event.key === '0')
+    ) {
         event.preventDefault();
     }
 });
-window.addEventListener('wheel', (event) => {
-    if (event.ctrlKey) {
-        event.preventDefault();
-    }
-}, { passive: false });
+
+window.addEventListener(
+    'wheel',
+    (event) => {
+        if (event.ctrlKey) {
+            event.preventDefault();
+        }
+    },
+    { passive: false }
+);
